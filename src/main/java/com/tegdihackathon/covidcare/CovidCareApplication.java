93c940d1d28b30e84ca8f75b4a04b1d22a800a62
KApolinario1120 <kevinsquared@gmail.com>
2022-11-17 17:47:06 -0500

    Spring initialized project

---
+++ src/main/java/com/tegdihackathon/covidcare/CovidCareApplication.java
@@ -0,0 +1,13 @@
+package com.tegdihackathon.covidcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tegdihackathon/covidcare/CovidCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tegdihackathon.covidcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

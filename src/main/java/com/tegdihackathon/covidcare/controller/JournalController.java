73ec22e194436504f63cc3f64ec96f768d58bb5a
ErdbeerenindenBergen <114823741+ErdbeerenindenBergen@users.noreply.github.com>
2022-11-18 18:36:20 -0500

    updating journalController

--- src/main/java/com/tegdihackathon/covidcare/controller/JournalController.java
+++ src/main/java/com/tegdihackathon/covidcare/controller/JournalController.java
@@ -2,6 +2,7 @@ package com.tegdihackathon.covidcare.controller;
 
 import com.tegdihackathon.covidcare.dao.JournalDao;
 import com.tegdihackathon.covidcare.model.Journal;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,20 +18,30 @@ public class JournalController {
         this.journalDao = journalDao;
     }
 
+    //Retrieve a journal entry by id
+    @RequestMapping(path = "/{journalId}", method = RequestMethod.GET)
+    public Journal getJournalByJournalId(@PathVariable int journalId) {
+        return journalDao.getJournalByJournalId(journalId);
+    }
+
+    //Get a journal entry by user id
+    @RequestMapping(path = "/user/{userId}", method = RequestMethod.GET)
+    public Journal getJournalByUserId(@PathVariable int userId) {
+        return journalDao.getJournalByUserId(userId);
+    }
+
+    //Update a journal entry
+//  boolean updateJournalEntry(Journal journal);
+    @RequestMapping(path = "/update/{journalId}", method = RequestMethod.PUT)
+    public boolean updateJournalEntry(@Valid @RequestBody Journal journal) {
+        return journalDao.updateJournalEntry(journal);
+    }
+
     @RequestMapping(path = "/submit", method = RequestMethod.POST)
     public void createJournalEntry(@Valid @RequestBody Journal journal) {
         journalDao.createJournal(journal);
     }
 
-
-
-//
-//    //Retrieve a journal entry by id
-//    Journal getJournalByJournalId(int journalId);
-//
-//    //Get a journal entry by user id
-//    Journal getJournalByUserId(int userId);
-//
 //    //Update a journal entry
 //    boolean updateJournalEntry(Journal journal);
 //
--- src/main/java/com/tegdihackathon/covidcare/dao/JdbcJournalDao.java
+++ src/main/java/com/tegdihackathon/covidcare/dao/JdbcJournalDao.java
@@ -47,7 +47,7 @@ public class JdbcJournalDao implements JournalDao {
     @Override
     public boolean updateJournalEntry(Journal journal) {
         String sql = "UPDATE public.journal SET tested_positive = ?, notes = ?, temperature = ? WHERE journal_id = ?";
-        int numberOfRows = jdbcTemplate.update(sql, journal.isTestedPositive(), journal.getNotes(), journal.getTemperature());
+        int numberOfRows = jdbcTemplate.update(sql, journal.isTestedPositive(), journal.getNotes(), journal.getTemperature(), journal.getJournalId());
         return numberOfRows == 1;
     }
 

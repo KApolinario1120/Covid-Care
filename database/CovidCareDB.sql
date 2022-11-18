e8a9d2d75b95ed43e911c0cb2a75eaba8f64b1b7
ErdbeerenindenBergen <114823741+ErdbeerenindenBergen@users.noreply.github.com>
2022-11-18 14:10:00 -0500

    updating create DB script
    deleted has_symptom,
    added symptom_type

--- database/CovidCareDB.sql
+++ database/CovidCareDB.sql
@@ -3,7 +3,8 @@ DROP TABLE IF EXISTS journal_symptom, symptom, journal, care_user;
 
 CREATE TABLE care_user (
     user_id serial NOT NULL primary key,
-    username varchar(40) NOT NULL
+    username varchar(40) NOT NULL,
+	user_password varchar(100) 
 );
 
 CREATE TABLE journal (
@@ -19,18 +20,8 @@ CREATE TABLE journal (
 CREATE TABLE symptom (
     symptom_id serial NOT NULL primary key,
     symptom_name varchar(100) NOT NULL,
-    has_symptom boolean NOT NULL DEFAULT false
+    symptom_type varchar(100) NOT NULL
 );
-	
--- CREATE TABLE journal_symptom (
---     journal_id integer NOT NULL,
---     symptom_id integer NOT NULL,
---     CONSTRAINT journal_symptom_pkey PRIMARY KEY (journal_id, symptom_id),
---     CONSTRAINT fk_journal_symptom_journal_id_references_journal_journal_id FOREIGN KEY (journal_id)
---         REFERENCES public.journal (journal_id),
---     CONSTRAINT fk_journal_symptom_symptom_id_references_symptom_symptom_id FOREIGN KEY (symptom_id)
---         REFERENCES public.symptom (symptom_id)
--- )
 
 CREATE TABLE journal_symptom (
     journal_id int NOT NULL,

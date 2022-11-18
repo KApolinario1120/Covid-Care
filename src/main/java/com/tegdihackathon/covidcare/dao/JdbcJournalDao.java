dfbc0736d1e716b4e10c2a5dff9437cf9adaa027
Brittany Weaver <brittanyweaver13@gmail.com>
2022-11-18 14:54:17 -0500

    working on journal daos pushing for code review

--- src/main/java/com/tegdihackathon/covidcare/dao/JdbcJournalDao.java
+++ src/main/java/com/tegdihackathon/covidcare/dao/JdbcJournalDao.java
@@ -2,6 +2,7 @@ package com.tegdihackathon.covidcare.dao;
 
 import com.tegdihackathon.covidcare.model.Journal;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,6 +18,7 @@ public class JdbcJournalDao implements JournalDao {
 
     @Override
     public Journal createJournal(Journal journal) {
+        String sql = "INSERT INTO public.journal(journal_id, user_id, journal_date, tested_positive, notes, temperature) VALUES (?,?,?,?,?,?)";
         return null;
     }
 
@@ -31,7 +33,10 @@ public class JdbcJournalDao implements JournalDao {
     }
 
     @Override
-    public Journal updateJournalEntry(String notes) {
+    public Journal updateJournalEntry(Journal journal) {
+        String sql = "UPDATE public.journal SET journal_id = ?, user_id = ?, tested_positive = ?, notes = ?, temperature = ? WHERE journal_id = ?";
+        int numberOfRows = jdbcTemplate.update(sql, journal.getJournalId(), journal.getUserId(), journal.isTestedPositive(), journal.getNotes(), journal.getTemperature(), journal.)
+
         return null;
     }
 
@@ -39,4 +44,17 @@ public class JdbcJournalDao implements JournalDao {
     public Journal deleteJournal(Journal journal, int journalId) {
         return null;
     }
+
+    private Journal mapRowToJournal(SqlRowSet result) {
+        Journal journal = new Journal();
+        journal.setJournalId(result.getInt("journal_id"));
+        journal.setUserId(result.getInt("user_id"));
+        journal.setJournalDate(result.getDate("journal_date").toLocalDate());
+        journal.setTestedPositive(result.getBoolean("tested_positive"));
+        journal.setNotes(result.getString("notes"));
+        journal.setTemperature(result.getDouble("temperature"));
+        return journal;
+
+    }
+
 }

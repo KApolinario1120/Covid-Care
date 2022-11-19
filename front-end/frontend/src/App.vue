62f3c2d67f41337b2868521ccae44674f10ca03a
David <dhshafer.73@gmail.com>
2022-11-18 21:06:40 -0600

    Working on front page

--- front-end/frontend/src/App.vue
+++ front-end/frontend/src/App.vue
@@ -1,44 +1,14 @@
 <template>
   <div id="app">
-    <b-navbar toggleable="lg" type="light" id="nav">
-        <router-link class="navbar-brand" v-bind:to="{ name: 'home' }"> Home </router-link>
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-        <b-collapse id="nav-collapse" is-nav>
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
-        
-            <b-nav-item-dropdown text="Recipes">
-              <b-dropdown-item><router-link v-bind:to="{ name: 'journals' }"> *View All Journal Entries* </router-link></b-dropdown-item>
-              <b-dropdown-item><router-link v-bind:to="{ name: 'create-journal' }"> Add Journal Entry </router-link></b-dropdown-item>
-            </b-nav-item-dropdown>
 
-            <router-link class="nav-link" v-bind:to="{ name: 'logout' }"> *Logout* </router-link>
+    <nav>
+      <router-link v-bind:to="{ name: 'home' }"> Home </router-link>
+      <router-link v-bind:to="{ name: 'journals' }"> *View All Journal Entries* </router-link>
+      <router-link v-bind:to="{ name: 'create-journal' }"> Add Journal Entry </router-link>
+    </nav>
 
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-    <router-view />
   </div>
 </template>
-
-<!-- <template>
-  <div id="app">
-    <img alt="Picture of Dice" src="./assets/dice.png">
-    <JournalCard gameName='Game Name Goes Here' sourceUrl='www.test.com' publisherType='publisher' genre='genre' mechanic='d20'/>
-    
-  </div>
-</template> -->
-
-<!-- <script>
-import JournalCard from './components/JournalCard.vue'
-
-export default {
-  name: 'App',
-  components: {
-    JournalCard
-  }
-}
-</script> -->
 
 <style>
 body{
--- front-end/frontend/src/router/index.js
+++ front-end/frontend/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/Home.vue'
-// Home view needs created/imported
 
 
 Vue.use(Router)

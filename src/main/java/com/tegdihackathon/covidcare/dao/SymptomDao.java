fbff9b6b9d670215b79b3f4af4f7878093699f35
ErdbeerenindenBergen <114823741+ErdbeerenindenBergen@users.noreply.github.com>
2022-11-18 19:20:25 -0500

    Update SymptomDao.java

--- src/main/java/com/tegdihackathon/covidcare/dao/SymptomDao.java
+++ src/main/java/com/tegdihackathon/covidcare/dao/SymptomDao.java
@@ -6,9 +6,6 @@ import java.util.List;
 
 public interface SymptomDao {
 
-    //Create a symptom - DON'T THINK WE NEED
-//    Symptom createSymptom(String symptomName);
-
     //Retrieve a list of all symptoms by Id
     List <Symptom> getAllSymptomsByJournalId(int journalId);
 
@@ -21,6 +18,4 @@ public interface SymptomDao {
     //Update symptom status
     void populateJournalSymptomTable(int journalId, List<Symptom> symptomList);
 
-    //Delete a symptom - DON'T THINK WE NEED
-//    Symptom deleteSymptom(String symptomName);
 }
